5c28b1976ed3e31c5f67b933154d6267930c28a3
Gayathri D R <99469991+gayathri257developer@users.noreply.github.com>
2023-01-04 17:42:39 +0530

    fixed error

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,19 +3,17 @@ import Carousel from "react-bootstrap/Carousel";
 
 const data = [
   "https://rukminim1.flixcart.com/flap/1680/280/image/1defb861e409319b.jpg?q=50",
-  "https://rukminim1.flixcart.com/flap/1680/280/image/685712c6cefb3c02.jpg?q=50",
-  "https://rukminim1.flixcart.com/flap/1680/280/image/685712c6cefb3c02.jpg?q=50",
 ];
 
 function UncontrolledExample() {
   return (
-    <Carousel style={{ marginTop: "2px", height: "200px" }}>
+  
       {data.map((img) => (
-        <Carousel.Item>
+      
           <img className="d-block w-100" src={img} alt="First slide" />
-        </Carousel.Item>
+       
       ))}
-    </Carousel>
+  
   );
 }
 
